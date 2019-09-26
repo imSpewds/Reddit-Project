2c84a5191b2477157e9ef0be780a6e01aa65747a
imSpewds <titorific10@gmail.com>
2019-09-26 09:16:56 -0700

    finished

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
 	$(".aw").hide();
+	$(".fun").hide();
+	$(".bread").hide();
 	
 	$("#banAw").click(function(){
 		$(".aw").fadeIn("slow");
+	});
+	
+	$("#banFun").click(function(){
+		$(".fun").fadeIn("slow");
+	});
+	
+	$("#banBread").click(function(){
+		$(".bread").fadeIn("slow");
 	});
 });
